ef867a340a74fcd412b6633fa8ea0c003a48f5bd
Raymond Li <raymond.countrylane@gmail.com>
2025-07-06 05:36:24 -0400

    Login/Signup v2

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCx00DmHMLmiDbfFUiBN3w5ueIxAzLqWus",
+  authDomain: "chitchat-254bc.firebaseapp.com",
+  projectId: "chitchat-254bc",
+  storageBucket: "chitchat-254bc.appspot.com",
+  messagingSenderId: "7406729436",
+  appId: "1:7406729436:web:4ceec3453f658398215c19"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
